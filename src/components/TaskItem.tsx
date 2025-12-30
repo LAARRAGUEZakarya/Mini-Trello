2325a64796f916c4cd461f9ab42484a3d803d018
LAARRAGUEZakarya <zakariya.laarrague@outlook.fr>
2025-12-30 12:27:16 +0100

    update task item component for better design using bootstrap

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -1,6 +1,5 @@
 import type { Task } from "../types";
 
-
 interface Props {
   task: Task;
   toggleTask: (id: number) => void;
@@ -9,11 +8,22 @@ interface Props {
 
 export default function TaskItem({ task, toggleTask, deleteTask }: Props) {
   return (
-    <li className={task.done ? "done" : ""}>
-      <span onClick={() => toggleTask(task.id)}>
+    <li className="list-group-item d-flex justify-content-between align-items-center">
+      <span
+        className={`pointer ${
+          task.done ? "text-decoration-line-through text-muted" : ""
+        }`}
+        onClick={() => toggleTask(task.id)}
+      >
         {task.title}
       </span>
-      <button onClick={() => deleteTask(task.id)}>❌</button>
+
+      <button
+        className="btn btn-sm btn-outline-danger"
+        onClick={() => deleteTask(task.id)}
+      >
+        ✕
+      </button>
     </li>
   );
 }
